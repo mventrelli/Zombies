7d24bc8e5c5c7e6bd312a980d7be4721b806f090
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-04-17 14:00:58 -0500

    Initial Commit

---
+++ Zombies/ZombiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZombiesApp.swift
+//  Zombies
+//
+//  Created by IIT PHYS 440 on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ZombiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
